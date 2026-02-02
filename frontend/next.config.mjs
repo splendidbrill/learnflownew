49dcfb61a4e99c9ae3c578cb0cec056ffff3a7a0
splendidbrill <splendidbrill@gmail.com>
2026-02-02 12:41:54 +0530

    add docker config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: "standalone",
     // We need to allow images from Supabase to load
     images: {
       remotePatterns: [
